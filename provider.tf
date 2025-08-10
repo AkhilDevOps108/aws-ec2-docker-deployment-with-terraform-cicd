a8a15aa1d1891ca93349a19c7809229d6de3c4e8
Akhil kumar <154546670+AkhilDevOps108@users.noreply.github.com>
2025-08-10 11:33:04 +0530

    2

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-akhil" # Your bucket name
+    bucket         = "terraform-state-akhil-20250810" # Your bucket name
     key            = "ec2-docker/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
