6bfa3c5488619903dcdf1dc1619993d24b54aad2
Akhil kumar <154546670+AkhilDevOps108@users.noreply.github.com>
2025-08-10 11:46:44 +0530

    5

--- variables.tf
+++ variables.tf
@@ -3,3 +3,18 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+variable "github_username" {
+  description = "GitHub username for Docker login"
+  type        = string
+}
+
+variable "github_token" {
+  description = "GitHub token for Docker login"
+  type        = string
+  sensitive   = true
+}
+
+variable "image_name" {
+  description = "Docker image name to deploy"
+  type        = string
+}
