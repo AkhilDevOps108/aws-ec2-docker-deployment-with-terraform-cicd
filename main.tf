82d0f55e2f422e8fa2c4185d79785aa192de01db
Akhil kumar <154546670+AkhilDevOps108@users.noreply.github.com>
2025-08-10 11:26:09 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,60 @@
+# Security Group
+resource "aws_security_group" "app_sg" {
+  name        = "app-sg"
+  description = "Allow HTTP and SSH"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Get Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "app_server" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t2.micro"
+  security_groups        = [aws_security_group.app_sg.name]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras enable docker
+              yum install -y docker
+              service docker start
+              docker login ghcr.io -u ${var.github_username} -p ${var.github_token}
+              docker pull ghcr.io/${var.github_username}/${var.image_name}:latest
+              docker run -d -p 80:80 ghcr.io/${var.github_username}/${var.image_name}:latest
+              EOF
+
+  tags = {
+    Name = "AppServer"
+  }
+}
